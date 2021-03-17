ec3d1cc97322a250dc58943299676b35a8da15e2
BlackhordeES <esingcomputacion@gmail.com>
2021-03-17 20:39:02 -0300

    Variables

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
-console.log ('Hola a todos');
-console.log (5+5);
+var nombre = 'Hernan';
+console.log (nombre);
+console.log(typeof(nombre));
+
+var edad = 30;
+console.log (edad);
+console.log (typeof(edad));
+
+var sueldo = 1900.99;
+console.log (sueldo);
+console.log(typeof(sueldo));
+
+var tieneTrabajo = false;
+console.log(tieneTrabajo);
+console.log(typeof(tieneTrabajo));
+
+var puesto;
+console.log (puesto);
+puesto = null;
+console.log (puesto);
