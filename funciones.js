fa883da8a9816cd00ed5c04424201ca6691dda14
BlackhordeES <esingcomputacion@gmail.com>
2021-03-18 18:53:54 -0300

    Arreglos

--- funciones.js
+++ funciones.js
@@ -5,7 +5,7 @@ var sumar = function(a,b,c=3)
 }
 var result = sumar (4, 5, 8);
 console.log (result);
-*/
+
 
 //funciones recursivas
 
@@ -18,4 +18,47 @@ var factorial = function (n)
 }
 
 console.log(factorial(12));
+
+
+// arreglos
+
+var nombres = ['hernan', 'paula', 'luis', 'marcos'];
+var vegetales = new Array ('tomate', 'lechuga', 'pepino');
+
+console.log(nombres[3]);
+
+nombres[0] = 'jose';
+vegetales[2]= 'nabo';
+
+console.log (nombres[0]);
+console.log (vegetales[2]);
+*/
+var nombres = ['hernan', 'paula', 'luis', 'marcos'];
+var vegetales = new Array ('tomate', 'lechuga', 'pepino');
+
+//console.log(nombres.length);
+for(var i=0; i<= nombres.length-1; i++)
+{
+    console.log(nombres[i]);
+}
+
+vegetales.forEach(function(elemento,indice) 
+{
+    console.log (elemento, indice);    
+})
+
+console.log(nombres);
+nombres.push ('pedro');
+console.log(nombres);
+nombres.unshift('marta');
+console.log(nombres);
+nombres.pop();
+console.log(nombres);
+nombres.shift();
+console.log(nombres);
+var pos = nombres.indexOf('hernan');
+console.log (pos);
+nombres.splice(2,2);
+console.log(nombres);
+
 
