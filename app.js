9bdb5f1e9f4663c95c871f1d2e4a088b34917758
BlackhordeES <esingcomputacion@gmail.com>
2021-03-18 16:25:14 -0300

    Operadores Matematicos

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*uso de variables
 var nombre = 'Hernan';
 console.log (nombre);
 console.log(typeof(nombre));
@@ -13,8 +14,32 @@ console.log(typeof(sueldo));
 var tieneTrabajo = false;
 console.log(tieneTrabajo);
 console.log(typeof(tieneTrabajo));
+//hola q tal este es un comentario
+asdasd123123
 
 var puesto;
 console.log (puesto);
 puesto = null;
 console.log (puesto);
+*/
+//operadores matematicos
+
+var edadAna, edadMaria, diferenciaEdad;
+var sumaEdades, yearAna, yearMaria, yearActual;
+
+edadAna = 34;
+edadMaria = 28;
+yearActual = 2021;
+
+diferenciaEdad = edadAna - edadMaria;
+sumaEdades = edadAna + edadMaria;
+
+yearAna = yearActual-edadAna;
+yearMaria = yearActual-edadMaria;
+
+console.log (diferenciaEdad);
+console.log (sumaEdades);
+console.log ('Año en el que nacio Ana'+ yearAna);
+console.log ('Año en el que nacio Maria'+ yearMaria);
+console.log (yearActual*5);
+console.log(yearActual/5);
